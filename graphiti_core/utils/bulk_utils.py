7da80bd056ed3590f516cb22fd249e9f000c1b14
guilherme.takata <guilherme.takata.ext@cibra.com>
2025-06-26 18:48:16 -0300

    Remove the default entity from the labels

--- graphiti_core/utils/bulk_utils.py
+++ graphiti_core/utils/bulk_utils.py
@@ -141,7 +141,7 @@ async def add_nodes_and_edges_bulk_tx(
         }
 
         entity_data.update(node.attributes or {})
-        entity_data['labels'] = list(set(node.labels + ['Entity']))
+        entity_data['labels'] = list(set(node.labels))
         nodes.append(entity_data)
 
     edges: list[dict[str, Any]] = []
